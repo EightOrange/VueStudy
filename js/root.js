9de42b6b0d09b32fad11e108fa66b072d759a267
eightorange <yzf804988395@163.com>
2021-10-12 18:06:17 +0800

    上传了一些基础文件

--- js/root.js
+++ js/root.js
@@ -1,17 +1,5 @@
 /* @Author:     yuzhuofan               */
 /* @Date:       2021-09-29/10:56:50     */
 /* @Description:测试页面1 javaScript部分  */
-const x = new Vue({
-    el: '#root',
-    data:function () {
-        return{
-            name:`hello,world!`,
-            age: `18`,
-
-        }
-    }
-
-})
-console.log(x);
 
 
